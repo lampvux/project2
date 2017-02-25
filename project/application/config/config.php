8bc73ddc16d0c3a781dc2cbd4e8c89df91f0885a
vanthongle <thong.lv.95@gmail.com>
2017-02-25 11:40:06 +0700

    fghj
    fghj

--- project/application/config/config.php
+++ project/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+date_default_timezone_set("Asia/Bangkok");
+$config['base_url'] = 'http://localhost/project/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +36,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
